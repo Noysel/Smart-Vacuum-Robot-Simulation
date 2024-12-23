da91fe1b2913bd7265930cb1fb718ec3a38750d0
Noysel <noysela55@gmail.com>
2024-12-23 12:09:15 +0200

    updated skeleton + Future

--- src/main/java/bgu/spl/mics/application/objects/Camera.java
+++ src/main/java/bgu/spl/mics/application/objects/Camera.java
@@ -4,16 +4,6 @@ package bgu.spl.mics.application.objects;
  * Represents a camera sensor on the robot.
  * Responsible for detecting objects in the environment.
  */
-import jave.util.List;
-enum status {
-    UP,
-    DOWN,
-    ERROR
-}
 public class Camera {
-    private int id;
-    private int frequency;
-    private enum status;
-    private List<DetectedObject> detectedObjectList;
-
+    // TODO: Define fields and methods.
 }
