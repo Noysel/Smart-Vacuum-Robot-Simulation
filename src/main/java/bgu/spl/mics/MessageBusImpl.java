f5d0026cd865003a166f02c0e06f3cb8a7a9a4bc
LiorLotan2 <liorlotan2@gmail.com>
2024-12-24 13:05:59 +0200

    upadtew 2

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -1,5 +1,10 @@
 package bgu.spl.mics;
 
+import java.util.Queue;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
  * Write your implementation here!
@@ -8,55 +13,100 @@ package bgu.spl.mics;
  */
 public class MessageBusImpl implements MessageBus {
 	
-	@Override
-	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
-		// TODO Auto-generated method stub
+	private List<QuadMSQ> listMsqs;
+	private int nextIndex;
 
+	public MessageBusImpl() {
+		listMsqs = new ArrayList<>();
+		nextIndex = 0;
+	}
+
+	@Override
+
+	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
+		m.subscribeEvent(type, null); // CHECKKKKK
+		for (int i = 0; i < listMsqs.size(); i++) {
+			if (listMsqs.get(i).getMicroService() == m) {
+				listMsqs.get(i).getEventSub().add(type);
+			}
+		}
 	}
 
 	@Override
 	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
-		// TODO Auto-generated method stub
+		m.subscribeBroadcast(type, null); // CHECKKKKK
+		for (int i = 0; i < listMsqs.size(); i++) {
+			if (listMsqs.get(i).getMicroService() == m) {
+				listMsqs.get(i).getBroadcastSub().add(type);
+			}
+		}
 
 	}
 
 	@Override
 	public <T> void complete(Event<T> e, T result) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 	@Override
 	public void sendBroadcast(Broadcast b) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < listMsqs.size(); i++) {
+			if (listMsqs.get(i).getBroadcastSub().contains(b)) {
+				listMsqs.get(i).getMessageQueue().add(b);
+				notifyAll();
+			}
+		}
 
 	}
-
 	
 	@Override
 	public <T> Future<T> sendEvent(Event<T> e) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int i = nextIndex; i < listMsqs.size() % listMsqs.size(); i++) {
+			if (listMsqs.get(i).getEventSub().contains(e.getClass()))  {
+				listMsqs.get(i).getMessageQueue().add(e);
+				nextIndex = i + 1;
+				notifyAll();
+				break;
+			}
+		}
+		return new Future<>();
 	}
 
 	@Override
 	public void register(MicroService m) {
-		// TODO Auto-generated method stub
-
+		listMsqs.add(new QuadMSQ(m));
 	}
 
 	@Override
-	public void unregister(MicroService m) {
-		// TODO Auto-generated method stub
-
+	public void unregister(MicroService m) { // CHECK IF NEEDED TO REMOVE REFERNECES
+		for (int i = 0; i < listMsqs.size(); i++) {
+			if (listMsqs.get(i).getMicroService() == m) {
+				listMsqs.remove(i);
+				break;
+			}
+		}
 	}
 
 	@Override
 	public Message awaitMessage(MicroService m) throws InterruptedException {
-		// TODO Auto-generated method stub
+		boolean isFound = false;
+		for (QuadMSQ quad : listMsqs) {
+			if (quad.getMicroService() == m) 
+				isFound = true;
+				while (quad.getMessageQueue().isEmpty()) {
+					try {
+						this.wait();
+					}
+					catch (InterruptedException e) {
+						Thread.currentThread().interrupt();
+					}
+				}
+				return (Message)quad.getMessageQueue().poll();
+		}
+		if (!isFound) {
+			throw new IllegalStateException();
+		}
 		return null;
 	}
-
-	
-
 }
