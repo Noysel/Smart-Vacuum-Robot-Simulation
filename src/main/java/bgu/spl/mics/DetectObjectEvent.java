60c401b8c41de95a814146b2e79196eedff81ae0
LiorLotan2 <liorlotan2@gmail.com>
2024-12-30 12:36:25 +0200

    UPDATE 9.1

--- src/main/java/bgu/spl/mics/DetectObjectEvent.java
+++ src/main/java/bgu/spl/mics/DetectObjectEvent.java
@@ -3,15 +3,16 @@ package bgu.spl.mics;
 import bgu.spl.mics.application.objects.CloudPoint;
 import bgu.spl.mics.application.objects.DetectedObject;
 import bgu.spl.mics.application.objects.TrackedObject;
+import bgu.spl.mics.application.objects.StampedDetectedObjects;
+
 
 public class DetectObjectEvent implements Event<Boolean> {
 
-    private DetectedObject obj;
-    private int currentTime;
-    public DetectObjectEvent(DetectedObject obj) {
+    private StampedDetectedObjects obj;
+    public DetectObjectEvent(StampedDetectedObjects obj) {
         this.obj = obj;
     }
-    public DetectedObject getDetectedObj() {
+    public StampedDetectedObjects getDetectedObj() {
         return this.obj;
     }
 }
